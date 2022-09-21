e091cc096aec8f0d75e2825069e6925c8d2f9605
Людмила Алтухова <luda.tamb@mail.ru>
2022-09-21 21:08:15 +0300

    refactor: correct main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
 import Header from "./components/Header";
+import Social from "./components/Social";
+import Skills from "./components/Skills";
+import ProjectsList from "./components/ProjectsList";
+
+const AppWrapper = styled.div`
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  max-width: 350px;
+`;
 
 const App = () => {
-  return <Header />;
+  return (
+    <AppWrapper>
+      <Header />
+      <Social />
+      <Skills />
+      <ProjectsList />
+    </AppWrapper>
+  );
 };
 
 export default App;
