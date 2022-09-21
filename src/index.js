4eac4bfcc6c5b13099635938cb8b3c67134f2dfb
Людмила Алтухова <luda.tamb@mail.ru>
2022-09-21 21:06:19 +0300

    refactor: add global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  color: #333;
+  font-family: "Helvetica Neue", sans-serif;
+}
+
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
